3c5b1fe5baf7a0d5236540a5d86a3cd902451977
Cande De Franco <candedefranco@Candes-MacBook-Pro.local>
2024-08-23 12:47:46 -0300

    hice modificaciones del ej 1 del tp2

--- src/leap_year.py
+++ src/leap_year.py
@@ -1,17 +1,17 @@
 # Replace the "ANSWER HERE" for your answer
 
-input1 = int(input('Ingrese un año: '))
+input1 : int = int(input('Ingrese un año: '))
 
-def is_leap_year(input1):
+def is_leap_year(input1) -> None:
     if (input1 % 100 == 0) and (input1 % 400 == 0):
-        return (f'El año {input1} es bisiesto')
+        print (f'El año {input1} es bisiesto')
     elif input1 % 4 == 0:
         if (input1 % 100 == 0):
-            return (f'El año {input1} no es bisiesto')
+            print (f'El año {input1} no es bisiesto')
         else:
-            return (f'El año {input1} es bisiesto')
+            print (f'El año {input1} es bisiesto')
     else:
-        return (f'El año {input1} no es bisiesto')
+        print (f'El año {input1} no es bisiesto')
     
 
-print(is_leap_year(input1))
+is_leap_year(input1)
--- src/maximums.py
+++ src/maximums.py
@@ -1,6 +1,6 @@
-def max_of_two(x, y):
+def max_of_two(x:int, y:int) -> int:
     """Given x and y, that are 2 numbers, return the biggest number."""
-    biggest = x
+    biggest : int = x
     if x >= y:
         return biggest
     else:
@@ -8,12 +8,12 @@ def max_of_two(x, y):
         return biggest
 
 # Replace the "ANSWER HERE" for your answer
-def max_of_three(x, y, z):
+def max_of_three(x: int, y: int, z: int):
     """Given x, y and z, that are 3 numbers, return the biggest number of the three."""
-    biggest_of_two = max_of_two(x,y)
+    biggest_of_two: int = max_of_two(x,y)
     if biggest_of_two >= z:
         return biggest_of_two
     else:
         return z
     
-print(max_of_two(5,4))
+print(max_of_two(3,2))
