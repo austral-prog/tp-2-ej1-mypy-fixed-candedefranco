4afc8163efe6a7c08910cca49558212284b1d4b3
Cande De Franco <candedefranco@Candes-MacBook-Pro.local>
2024-08-30 12:44:01 -0300

    modifique de nuevo el tp2 ej1

--- src/leap_year.py
+++ src/leap_year.py
@@ -1,15 +1,15 @@
 # Replace the "ANSWER HERE" for your answer
 
-def is_leap_year() -> None:
+def is_leap_year() -> str:
     input1 : int = int(input('Ingrese un año: '))
     if (input1 % 100 == 0) and (input1 % 400 == 0):
-        print (f'El año {input1} es bisiesto')
+        return f'El año {input1} es bisiesto'
     elif input1 % 4 == 0:
         if (input1 % 100 == 0):
-            print (f'El año {input1} no es bisiesto')
+            return f'El año {input1} no es bisiesto'
         else:
-            print (f'El año {input1} es bisiesto')
+            return f'El año {input1} es bisiesto'
     else:
-        print (f'El año {input1} no es bisiesto')
+        return f'El año {input1} no es bisiesto'
     
 
